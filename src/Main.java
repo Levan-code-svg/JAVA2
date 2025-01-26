4a5e4a75cea416a961095a3bcda8b24c85860933
Levan <kft77736@gmail.com>
2025-01-26 16:43:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 17468;
+        int pricePerMile = 20;
+
+        int miles = ticketPrice / pricePerMile;
+        System.out.println(" Вам начисленно " + miles + " бонусных (-ые / -ая) миль (-и / -я)");
+    }
+}
